0606d3115d4713348480f51e3d31bb7baec77537
liftedkilt <williamdforsyth@gmail.com>
2017-05-01 20:58:45 -0700

    Clean slate for magnet

--- controller/index.js
+++
@@ -1,4 +0,0 @@
-'use strict'
-
-exports.local = require('./local.js');
-exports.cloudflare = require('./cloudflare.js');
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const controller = require('../controller');
-
-
-server.get('/zones/:zone_identifier/dns_records', controller.local.list_dns_records);
-
-server.post('/zones/:zone_identifier', controller.local.create_dns_record);
