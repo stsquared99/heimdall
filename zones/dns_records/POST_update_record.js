1a30874601388f38e83769517287b9d9df051dce
liftedkilt <williamdforsyth@gmail.com>
2017-05-09 11:56:27 -0700

    Fix Regression
    Updates weren't occuring in local DB

--- zones/dns_records/POST_update_record.js
+++ zones/dns_records/POST_update_record.js
@@ -9,7 +9,9 @@ export const route = {
 };
 
 export default async(req, res) => {
-	log.debug({req: req}, 'received request');
+	log.debug({
+		req: req
+	}, 'received request');
 	let error = validate.reqParams(req, 'cannot update record - missing required parameters');
 
 	if (error !== '') {
@@ -20,63 +22,53 @@ export default async(req, res) => {
 		data.zoneId = req.params.zone_identifier;
 
 		await Records.findOneAndUpdate({
-
-			id: req.params.identifier,
-			zoneId: req.params.zone_identifier,
-		}, {
-			data,
-		},
-		{
-			new: true,
-		})
-		.then(function (record) {
-			let generatedRecord = cloudflare.DNSRecord.create(data);
-
-			log.info(generatedRecord);
-
-			let editedRecord = {
-				'name': generatedRecord.name,
-				'content': generatedRecord.content,
-				'type': generatedRecord.type,
-				'ttl': generatedRecord.ttl,
-				'proxied': generatedRecord.proxied,
-			};
-
-			log.info(editedRecord);
-			return cf.editDNS(generatedRecord);
-		})
-		.then(async function(record) {
-			log.info(record.id);
-			data.id = record.id;
-
-			// Write record to DB
-			let entry = new Records(data);
-			await entry.save();
-			return data;
-		})
-		.then(function(record) {
-			log.info('record updated');
-			res.status(200).json({
-				result: 'success',
-				message: 'Record updated',
-				info: {
-					content: req.body.content,
-					name: req.body.name,
-					proxied: req.body.proxied,
-					ttl: req.body.ttl,
-					type: req.body.type,
+					id: data.id,
+					zoneId: data.zoneId,
 				},
-			});
-		})
-		.catch(function(error) {
-			log.error({error: error}, 'Error updating a record');
-			res.status(500).json({
-				result: 'error',
-				message: 'Cannot update record',
-				info: {
+				data, {
+					new: true,
+				})
+			.then(function(record) {
+				let generatedRecord = cloudflare.DNSRecord.create(data);
+
+				log.info(generatedRecord);
+
+				let editedRecord = {
+					'name': generatedRecord.name,
+					'content': generatedRecord.content,
+					'type': generatedRecord.type,
+					'ttl': generatedRecord.ttl,
+					'proxied': generatedRecord.proxied,
+				};
+
+				log.info(editedRecord);
+				return cf.editDNS(generatedRecord);
+			})
+			.then(function(record) {
+				log.info('record updated');
+				res.status(200).json({
+					result: 'success',
+					message: 'Record updated',
+					info: {
+						content: req.body.content,
+						name: req.body.name,
+						proxied: req.body.proxied,
+						ttl: req.body.ttl,
+						type: req.body.type,
+					},
+				});
+			})
+			.catch(function(error) {
+				log.error({
 					error: error,
-				},
+				}, 'Error updating a record');
+				res.status(500).json({
+					result: 'error',
+					message: 'Cannot update record',
+					info: {
+						error: error,
+					},
+				});
 			});
-		});
 	}
 };
