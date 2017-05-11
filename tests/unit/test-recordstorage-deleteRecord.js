56bcf86263de6e3ff079c17a21f3be63880c1ada
liftedkilt <williamdforsyth@gmail.com>
2017-05-11 12:34:25 -0700

    refactor deletion, add unit test for remapping params

---
+++ tests/unit/test-recordstorage-deleteRecord.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let assert = require('assert');
+let Remap = require('../../lib/remap');
+
+exports.it_should_return_an_ID_and_zoneId = function(done) {
+	let req = {
+				params: {
+					zone_identifier: '235k6jh34lk6g124kjg51lk4g512lk4g',
+					identifier: '12lk4j51lk24jg512klj51l2kj12klj5',
+				},
+			};
+
+	let desiredResult = {
+		id: '12lk4j51lk24jg512klj51l2kj12klj5',
+		zoneId: '235k6jh34lk6g124kjg51lk4g512lk4g',
+	};
+
+	assert.deepEqual(Remap.reqParams(req), desiredResult);
+	return done();
+};
