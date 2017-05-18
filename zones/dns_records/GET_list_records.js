0a3cc005bcc713cc6e1d05df505cf617ea69c5c9
liftedkilt <williamdforsyth@gmail.com>
2017-05-18 07:49:28 -0700

    Fixes broken build

--- zones/dns_records/GET_list_records.js
+++ zones/dns_records/GET_list_records.js
@@ -11,7 +11,7 @@ export const route = {
 export default async(req, res) => {
 	log.debug({req: req}, 'received request');
 	try {
-		let records = await MongoRecords.getRecords(req.zone_identifier);
+		let records = await MongoRecords.getRecords(req.params.zone_identifier);
 		log.info('The records were retrieved for zone: %s', req.params.zone_identifier);
 		return records;
 	} catch (error) {
